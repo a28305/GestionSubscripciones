821100f87d7e64d0f627ecdead66891a1a420660
a28305 <a28305@svalero.com>
2025-11-25 17:23:14 +0100

    Alguna clase

---
+++ models/Usuarios.cs
@@ -0,0 +1,17 @@
+namespace ENTREGA_1EV.models
+{
+    public class Usuario
+    {
+       
+        public int ID_Usuario { get; set; } // int (Clave Primaria)
+        public string Email { get; set; } 
+        public string NombrePerfil { get; set; } 
+        public int DispositivosActivos { get; set; } 
+        public decimal MontoPagadoAcumulado { get; set; } 
+        public bool PremiumActivo { get; set; } 
+        public DateTime FechaRegistro { get; set; } 
+
+        // Relación Muchos a Uno: Un Usuario tiene un PlanSuscripcion
+        public PlanSuscripcion PlanActual { get; set; }
+    }
+}
